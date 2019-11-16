60b14ea921d4bb64a36a2f93ddf8cfb7292323ba
Shanti Stewart <55906822+shantistewart@users.noreply.github.com>
2019-11-15 17:18:15 -0800

    Added header file for Minion test.

---
+++ projects/stewars2/dominion/unittest2.h
@@ -0,0 +1,16 @@
+//
+//  unittest2.h.h
+//  CS 362 Dominion
+//
+//  Created by Shanti Stewart on 11/15/19.
+//  Copyright © 2019 CS 362. All rights reserved.
+//
+
+#ifndef unittest2.h
+#define unittest2.h
+
+
+int test_minion(void);
+
+
+#endif /* unittest2.h */
