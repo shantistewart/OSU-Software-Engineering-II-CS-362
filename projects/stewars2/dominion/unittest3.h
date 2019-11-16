db4fc44cc74763b14bd31dd5504b7d6c2d80ab44
Shanti Stewart <55906822+shantistewart@users.noreply.github.com>
2019-11-15 17:20:42 -0800

    Create unittest3.h

---
+++ projects/stewars2/dominion/unittest3.h
@@ -0,0 +1,16 @@
+//
+//  unittest3.h
+//  CS 362 Dominion
+//
+//  Created by Shanti Stewart on 11/15/19.
+//  Copyright © 2019 CS 362. All rights reserved.
+//
+
+#ifndef unittest3.h
+#define unittest3.h
+
+
+int test_ambassador(void);
+
+
+#endif /* unittest3.h */
