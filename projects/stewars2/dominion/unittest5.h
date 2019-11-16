da270fd3b55c9583262af29587c9d835f2f02d08
Shanti Stewart <55906822+shantistewart@users.noreply.github.com>
2019-11-15 17:26:43 -0800

    Added header file for Mine test.

---
+++ projects/stewars2/dominion/unittest5.h
@@ -0,0 +1,16 @@
+//
+//  unittest5.h
+//  CS 362 Dominion
+//
+//  Created by Shanti Stewart on 11/15/19.
+//  Copyright © 2019 CS 362. All rights reserved.
+//
+
+#ifndef unittest5.h
+#define unittest5.h
+
+
+int test_mine(void);
+
+
+#endif /* unittest5.h */
