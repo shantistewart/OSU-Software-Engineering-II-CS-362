025e4b4d02168d759fc3e97fd6984982a9c537da
Shanti Stewart <55906822+shantistewart@users.noreply.github.com>
2019-11-15 17:29:37 -0800

    Added header file for Tribute test.

---
+++ projects/stewars2/dominion/unittest4.h
@@ -0,0 +1,16 @@
+//
+//  unittest4.h
+//  CS 362 Dominion
+//
+//  Created by Shanti Stewart on 11/15/19.
+//  Copyright © 2019 CS 362. All rights reserved.
+//
+
+#ifndef unittest4.h
+#define unittest4.h
+
+
+int test_tribute(void);
+
+
+#endif /* unittest4.h */
